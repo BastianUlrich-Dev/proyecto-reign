c474bf3dfa2991abc4212d1c551a8363eb87d6ce
Bastian Jorquera <bastianjorquera8@gmail.com>
2022-03-13 12:18:34 -0300

    Se agrego importacion de HttpClientModule y paginacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { NavComponent } from './components/nav/nav.component';
 import { NoticiaComponent } from './components/noticia/noticia.component';
 import { NoticiasComponent } from './components/noticias/noticias.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { PaginacionComponent } from './components/paginacion/paginacion.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     NoticiaComponent,
-    NoticiasComponent
+    NoticiasComponent,
+    PaginacionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
