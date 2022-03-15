718c9a4606e14bf60b479b0f9f9fd4e4d25b85e4
Bastian Jorquera <bastianjorquera8@gmail.com>
2022-03-15 01:19:39 -0300

    Se corrigieron errores

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,41 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Post } from 'src/app/models/news.model';
 
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
-  styleUrls: ['./noticia.component.scss']
+  styleUrls: ['./noticia.component.scss'],
 })
 export class NoticiaComponent implements OnInit {
-
   iconReloj = './../../../assets/images/iconmonstr-time-2.svg';
   iconLike = './../../../assets/images/iconmonstr-favorite-3.svg';
   iconoDisLike = './../../../assets/images/iconmonstr-favorite-2.svg';
 
-  imagenActiva = false;
+  @Input()
+  active?: boolean = false;
 
-  @Input() noticia: Post= {
+  @Output() toggleLike = new EventEmitter();
+  @Input() noticia: Post = {
     author: '',
     story_title: '',
     story_url: '',
     created_at: '',
-    objectID: ''
+    objectID: '',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  imagenToggle() {
+    this.active = !this.active;
   }
 
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-
-  imagenToggle(){
-    this.imagenActiva = !this.imagenActiva;
-  }
-
-  activeImg(){
+  activeImg() {
     this.imagenToggle();
     // console.log(this.imagenToggle());
+    this.toggleLike.emit();
   }
-
 }
