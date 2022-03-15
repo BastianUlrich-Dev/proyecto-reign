f191075bc154add1027f5d66b4eae0d900066126
Bastian Jorquera <bastianjorquera8@gmail.com>
2022-03-14 22:13:41 -0300

    Se agrego paginacion y scroll

--- src/app/components/noticias/noticias.component.ts
+++ src/app/components/noticias/noticias.component.ts
@@ -1,73 +1,134 @@
 import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import { faChevronDown, faDog } from '@fortawesome/free-solid-svg-icons';
-import { BehaviorSubject, switchMap } from 'rxjs';
+import { faChevronDown, faChevronLeft, faChevronRight, faDog } from '@fortawesome/free-solid-svg-icons';
+import { BehaviorSubject, combineLatest, map, switchMap, tap, withLatestFrom } from 'rxjs';
 import { Post } from 'src/app/models/news.model';
 import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-noticias',
   templateUrl: './noticias.component.html',
-  styleUrls: ['./noticias.component.scss']
+  styleUrls: ['./noticias.component.scss'],
 })
 export class NoticiasComponent implements OnInit {
-
   @ViewChild('parrafo') title: ElementRef | undefined;
   faDog = faDog;
   chevronDown = faChevronDown;
+  chevronLeft = faChevronLeft;
+  chevronRight = faChevronRight;
   opcionSeleccionada = false;
   cboActivo = false;
 
   options = [
     {
-      logo: "./../../../assets/images/image-138.png",
-      text: "Angular"
+      logo: './../../../assets/images/image-138.png',
+      text: 'Angular',
     },
     {
-      logo: "./../../../assets/images/image-140.png",
-      text: "React"
+      logo: './../../../assets/images/image-140.png',
+      text: 'React',
     },
     {
-      logo: "./../../../assets/images/image-141.png",
-      text: "Vuejs"
+      logo: './../../../assets/images/image-141.png',
+      text: 'Vuejs',
     },
   ];
 
-  params = new BehaviorSubject({ query: "", page: "0" });
+  paginas = [
+    {
+      numero: 1,
+    },
+    {
+      numero: 2,
+    },
+    {
+      numero: 3,
+    },
+    {
+      numero: 4,
+    },
+    {
+      numero: 5,
+    },
+    {
+      numero: 6,
+    },
 
-  news:Post[] = [];
+  ]
 
-  constructor(
-    private newsService: NewsService
-  ) { }
+  params = new BehaviorSubject({ query: '', page: '0' });
 
-  ngOnInit(): void {
-    this.params
-    .pipe(switchMap(params => this.newsService.getPosts(params)))
-    .subscribe(news => {
-      this.news = news;
-    });
+  news: Post[] = [];
+
+
+
+  query$ = new BehaviorSubject(localStorage.getItem("query") ?? "");
+  page$ = new BehaviorSubject(localStorage.getItem("page") ?? "0");
+  likedPosts$ = new BehaviorSubject(JSON.parse(localStorage.getItem("likedPosts") ?? "[]"));
+
+  page = "0";
+  isLoading = false;
+
+  // query = new Subject("");
+  // isLoading = new BehaviorSubject(false);
+
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit() {
+    combineLatest(
+      this.query$.pipe(tap((query) => localStorage.setItem("query", query))),
+      this.page$.pipe(
+        tap((page) => {
+          this.page = page;
+          localStorage.setItem("page", page);
+        })
+      ),
+      this.likedPosts$.pipe(
+        tap((likedPosts) => localStorage.setItem("likedPosts", likedPosts))
+      )
+    ).pipe(
+      switchMap(([query, page, likedPosts]) =>
+      this.newsService.getPosts({ query, page }).pipe(
+        map((posts) =>
+          [...this.news, ...posts.map((post) => ({
+            ...post,
+            isFavorite: likedPosts.includes(post.objectID),
+          }))]
+        ))
+      )
+      ).subscribe(news => {
+        this.news = news;
+      })
+  }
+  nextPage() {
+    this.page$.next(String(Number(this.page) + 1));
+    console.log(this.page$);
   }
 
-  change():void{
+  selectOption(option: string) {
+    this.query$.next(option);
+  }
+
+  goToPage(page: number) {
+    this.page$.next(String(page));
+  }
+
+  change(): void {
     const asTitle = this.title?.nativeElement;
     console.log(asTitle);
   }
 
-  toggleCboActivo(){
+  toggleCboActivo() {
     this.cboActivo = !this.cboActivo;
   }
 
-  toggleButton(){
+  toggleButton() {
     this.opcionSeleccionada = !this.opcionSeleccionada;
   }
 
-  activarCbo(){
+  activarCbo() {
     this.toggleButton();
     this.toggleCboActivo();
   }
 
-  selectOption(option: string) {
-    this.params.next({ query: option, page: "0" });
-  }
 
 }
