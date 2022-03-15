f2efb312ab5d53f438e0162100e5722c104301dd
Bastian Jorquera <bastianjorquera8@gmail.com>
2022-03-14 22:14:38 -0300

    Se agrego interpolation

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -10,18 +10,32 @@ export class NoticiaComponent implements OnInit {
 
   iconReloj = './../../../assets/images/iconmonstr-time-2.svg';
   iconLike = './../../../assets/images/iconmonstr-favorite-3.svg';
+  iconoDisLike = './../../../assets/images/iconmonstr-favorite-2.svg';
+
+  imagenActiva = false;
 
   @Input() noticia: Post= {
     author: '',
     story_title: '',
     story_url: '',
     created_at: '',
+    objectID: ''
   }
 
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  imagenToggle(){
+    this.imagenActiva = !this.imagenActiva;
+  }
+
+  activeImg(){
+    this.imagenToggle();
+    // console.log(this.imagenToggle());
   }
 
 }
